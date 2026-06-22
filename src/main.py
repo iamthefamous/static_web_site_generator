cfaaf4ee95da40ae4a6d657d7d88ddd89ad69589
Asylbek Zhunusov <zhunusov@proton.me>
2026-06-22 12:46:22 +0600

    final changes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,32 @@
-from textnode import TextNode
-def main():
-    new_text_node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
-    print(new_text_node)
+import os
+import shutil
+import sys
 
-if __name__ == "__main__":
-    main()
+from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./docs"
+dir_path_content = "./content"
+template_path = "./template.html"
+default_basepath = "/"
+
+
+def main() -> None:
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
+
+
+main()
